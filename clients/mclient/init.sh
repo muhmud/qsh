bd92917f2a5601b40d25cf77d7e103a3d9972512
Muhmud Ahmad <muhmud.ahmad@googlemail.com>
2021-03-14 00:12:11 +0800

    re-org

--- clients/mclient/init.sh
+++ clients/mclient/init.sh
@@ -10,10 +10,3 @@ OUTPUT_FILE=$6
 echo -e "select 'INITIALIZED' as \"qsh ($QSH_SQL_CLIENT)\";" > "$OUTPUT_FILE";
 tmux send-keys -t "$PANE_ID" "C-m";
 
-# Force the pager to be the QSH pager
-tmux send-keys -t $PANE_ID "\\| $QSH_PAGER_COMMAND";
-tmux send-keys -t $PANE_ID "C-m";
-
-# Clear the screen
-tmux send-keys -t $PANE_ID "C-l";
-
