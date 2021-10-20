3ab9a44d365db3ca0a509420b0ada59d8c43aa76
Muhmud Ahmad <muhmud.ahmad@toptal.com>
2021-10-20 13:26:13 +0500

    qsh-rlwrap

--- scripts/rlwrap.sh
+++ scripts/rlwrap.sh
@@ -2,5 +2,11 @@
 
 QSH_SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-rlwrap -a -A -m -N -n -C qsh -t dumb -z "$QSH_SCRIPTS/qsh-rlwrap" -- $*
+[[ -z "$QSH_RLWRAP_SHELL_COMMAND" ]] && QSH_RLWRAP_SHELL_COMMAND="!>>"
+[[ -z "$QSH_RLWRAP_PAGER" ]] && QSH_RLWRAP_PAGER="$QSH_SCRIPTS/qsh-pager"
+[[ ! -z "$QSH_PROMPT" ]] && QSH_RLWRAP_PROMPT=-S "$QSH_PROMPT"
+
+QSH_RLWRAP_SHELL_COMMAND="$QSH_RLWRAP_SHELL_COMMAND" \
+QSH_RLWRAP_PAGER="$QSH_RLWRAP_PAGER" \
+  rlwrap -a -A -m -N -n -C qsh -t dumb -z "$QSH_SCRIPTS/qsh-rlwrap" $QSH_RLWRAP_PROMPT -- $*
 
