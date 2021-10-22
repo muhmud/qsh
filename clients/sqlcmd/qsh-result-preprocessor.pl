335318efc8958333e348505e241a7b6c18022a45
Muhmud Ahmad <muhmud.ahmad@toptal.com>
2021-10-22 12:21:35 +0500

    sqlcmd fixes

--- clients/sqlcmd/qsh-result-preprocessor.pl
+++ clients/sqlcmd/qsh-result-preprocessor.pl
@@ -20,6 +20,38 @@ my $previous_line = "";
 
 my $headers_initialized = 0;
 my $output_started = 0;
+my $last_line_found = 0;
+
+sub is_valid_line {
+  my $is_valid = 1;
+  my $index = 0;
+  my $start = 0;
+
+  if ($line =~ /^\([0-9]+ rows affected\)$/) {
+    $last_line_found = 1;
+    return -1;
+  }
+
+  if ($last_line_found == 1) {
+    return 0;
+  }
+
+  foreach my $position (@column_max_size) {
+    if (++$index != $column_count) {
+      if ($start == 0) {
+        $start = $position;
+      } else {
+        $start += $position + 1;
+      }
+      if (substr($line, $start, 1) ne $column_separator) {
+        $is_valid = 0;
+        last;
+      }
+    }
+  }
+
+  return $is_valid;
+}
 
 while(<>) {
   $previous_line = $line;
@@ -52,26 +84,9 @@ while(<>) {
       $headers_initialized = 1;
     }
   } else {
-    my $is_valid = 1;
-    my $index = 0;
-    my $start = 0;
-    foreach my $position (@column_max_size) {
-      if (++$index != $column_count) {
-        if ($start == 0) {
-          $start = $position;
-        } else {
-          $start += $position + 1;
-        }
-        if (substr($line, $start, 1) ne $column_separator) {
-          $is_valid = 0;
-          break;
-        }
-      }
-    }
-
-    if ($is_valid == 1) {
-      $index = 0;
-      $start = 0;
+    if (is_valid_line() == 1) {
+      my $index = 0;
+      my $start = 0;
       foreach my $size (@column_max_size) {
         my $current_column = substr($line, $start, $size);
         $current_column =~ s/\s+$//g;
@@ -102,6 +117,7 @@ if ($headers_initialized == 0) {
 }
 
 open(INPUT, "<", $tmp_filename) or die $!;
+$last_line_found = 0;
 
 while(<INPUT>) {
   $previous_line = $line;
@@ -143,26 +159,12 @@ while(<INPUT>) {
       $output_started = 1;
     } 
   } else {
-    my $is_valid = 1;
-    my $index = 0;
-    my $start = 0;
-    foreach my $position (@column_max_size) {
-      if (++$index != $column_count) {
-        if ($start == 0) {
-          $start = $position;
-        } else {
-          $start += $position + 1;
-        }
-        if (substr($line, $start, 1) ne $column_separator) {
-          $is_valid = 0;
-          break;
-        }
-      }
-    }
+    $line =~ s/\r//g;
 
-    $start = 0;
-    $index = 0;
+    my $is_valid = is_valid_line();
     if ($is_valid == 1) {
+      my $start = 0;
+      my $index = 0;
       foreach my $size (@column_new_size) {
         my $current_column = substr($line, $start, $size);
 
@@ -174,11 +176,16 @@ while(<INPUT>) {
         $start += $column_max_size[$index] + 1;
         $index++;
       }
-    } else {
-      print $line;
-    }
 
-    print "\n";
+      print "\n";
+    } else {
+      if ($is_valid == -1) {
+        print $line;
+        last;
+      }
+
+      print "$line\n";
+    }
   }
 }
 
