6d8151ece95124ec0b3b60a94d87ee547f5c7256
Muhmud Ahmad <muhmud.ahmad@toptal.com>
2021-10-24 12:14:24 +0500

    multiple result set handling for sqlcmd cleanup

--- clients/sqlcmd/qsh-output-finalizer.pl
+++ clients/sqlcmd/qsh-output-finalizer.pl
@@ -13,14 +13,15 @@ if (!$column_separator) {
 my @column_min_size = ();
 my @column_max_size = ();
 my @column_new_size = ();
-my $column_count = 0;
+my @column_counts = ();
+my @column_starts = ();
 
 my $line = "";
 my $previous_line = "";
 
-my $headers_initialized = 0;
-my $output_started = 0;
+my $line_count = -1;
 my $last_line_found = 0;
+my $current_result = 0;
 
 sub is_valid_line {
   my $is_valid = 1;
@@ -36,7 +37,13 @@ sub is_valid_line {
     return 0;
   }
 
-  foreach my $position (@column_max_size) {
+  if ($current_result == 0) {
+    return 0;
+  }
+
+  my $max_sizes = $column_max_size[$current_result - 1];
+  my $column_count = $column_counts[$current_result - 1];
+  foreach my $position (@$max_sizes) {
     if (++$index != $column_count) {
       if ($start == 0) {
         $start = $position;
@@ -56,50 +63,66 @@ sub is_valid_line {
 while(<>) {
   $previous_line = $line;
   $line = $_;
+  $line_count++;
 
   print $tmp $line;
 
-  if ($headers_initialized == 0) {
-    if ($line =~ /^([-]+[${column_separator}])*[-]+\s$/) {
-      my @headers = split(/[${column_separator}]+/, $previous_line);
-      my @separators = split(/[${column_separator}]/, $line);
+  if ($line =~ /^([-]+[${column_separator}])*[-]+\s$/) {
+    # New result set header line found
+    push(@column_starts, $line_count - 1);
 
-      my $index = 0;
-      foreach my $header (@headers) {
-        $header =~ s/^\s+|\s+$//g;
-        @column_min_size[$index++] = length($header);
-      }
+    my @headers = split(/[${column_separator}]+/, $previous_line);
+    my @separators = split(/[${column_separator}]/, $line);
 
-      $index = 0;
-      foreach my $separator (@separators) {
-        $separator =~ s/^\s+|\s+$//g;
-        @column_max_size[$index++] = length($separator);
-      }
-
-      $column_count = scalar @column_min_size;
-      if ($column_count != scalar(@column_max_size)) {
-        die "column count mismatch!";
-      }
-
-      $headers_initialized = 1;
+    my @min_sizes = ();
+    my $index = 0;
+    foreach my $header (@headers) {
+      $header =~ s/^\s+|\s+$//g;
+      @min_sizes[$index++] = length($header);
     }
-  } else {
+
+    push(@column_min_size, [ @min_sizes ]);
+
+    my @max_sizes = ();
+    $index = 0;
+    foreach my $separator (@separators) {
+      $separator =~ s/^\s+|\s+$//g;
+      @max_sizes[$index++] = length($separator);
+    }
+
+    push(@column_max_size, [ @max_sizes ]);
+
+    my $column_count = scalar(@min_sizes);
+    if ($column_count != scalar(@max_sizes)) {
+      die "column count mismatch!";
+    }
+    push(@column_counts, $column_count);
+
+    my @new_sizes = ();
+    push(@column_new_size, [ @new_sizes ]);
+
+    $current_result++;
+  } elsif ($current_result > 0){
     if (is_valid_line() == 1) {
       my $index = 0;
       my $start = 0;
-      foreach my $size (@column_max_size) {
+
+      my $min_sizes = @column_min_size[$current_result - 1];
+      my $max_sizes = @column_max_size[$current_result - 1];
+      my $new_sizes = @column_new_size[$current_result - 1];
+      foreach my $size (@$max_sizes) {
         my $current_column = substr($line, $start, $size);
         $current_column =~ s/\s+$//g;
         my $current_column_length = length($current_column);        
 
-        my $column_size = $column_new_size[$index];
+        my $column_size = @$new_sizes[$index];
         if (!$column_size) {
-          $column_new_size[$index] = $column_min_size[$index];
-          $column_size = $column_new_size[$index];
+          @$new_sizes[$index] = @$min_sizes[$index];
+          $column_size = @$new_sizes[$index];
         }
 
-        if (!$column_size || $column_size < $current_column_length) {
-          $column_new_size[$index] = $current_column_length;
+        if ($column_size < $current_column_length) {
+          @$new_sizes[$index] = $current_column_length;
         }
 
         $start += $size + 1;
@@ -111,61 +134,77 @@ while(<>) {
 
 close $tmp;
 
-if ($headers_initialized == 0) {
+if ($current_result == 0) {
   copy($tmp_filename, \*STDOUT);
   exit 0;
 }
 
 open(INPUT, "<", $tmp_filename) or die $!;
 $last_line_found = 0;
+$line_count = -1;
+$next_result_line_number = @column_starts[0];
+$current_result = 0;
 
 while(<INPUT>) {
   $previous_line = $line;
   $line = $_;
+  $line_count++;
 
-  if ($output_started == 0) {
-    if ($line =~ /^([-]+[${column_separator}])*[-]+\s$/) {
-      my $index = 0;
-      my $start = 0;
-      foreach my $size (@column_new_size) {
-        my $current_column = substr($previous_line, $start, $size);
+  if ($line_count == $next_result_line_number) {
+    # Continue on to the next line
+    next;
+  }
 
-        if ($index != 0) {
-          print "|";
-        }
-        print " $current_column ";
+  if ($line_count == $next_result_line_number + 1) {
+    # this is a separator line
+    my $index = 0;
+    my $start = 0;
 
-        $start += $column_max_size[$index] + 1;
-        $index++;
+    my $min_sizes = @column_min_size[$current_result];
+    my $max_sizes = @column_max_size[$current_result];
+    my $new_sizes = @column_new_size[$current_result];
+    foreach my $size (@$new_sizes) {
+      my $current_column = substr($previous_line, $start, $size);
+
+      if ($index != 0) {
+        print "|";
       }
+      print " $current_column ";
 
-      print "\n";
+      $start += @$max_sizes[$index] + 1;
+      $index++;
+    }
 
-      $index = 0;
-      $start = 0;
-      foreach my $size (@column_new_size) {
-        my $current_column = substr($line, $start, $size);
+    print "\n";
 
-        if ($index != 0) {
-          print "+";
-        }
-        print "-$current_column-";
+    $index = 0;
+    $start = 0;
+    foreach my $size (@$new_sizes) {
+      my $current_column = substr($line, $start, $size);
 
-        $start += $column_max_size[$index] + 1;
-        $index++;
+      if ($index != 0) {
+        print "+";
       }
+      print "-$current_column-";
 
-      print "\n";
-      $output_started = 1;
-    } 
-  } else {
+      $start += @$max_sizes[$index] + 1;
+      $index++;
+    }
+
+    $current_result++;
+    $next_result_line_number = @column_starts[$current_result];
+    print "\n";
+  } elsif ($current_result > 0){
     $line =~ s/\r//g;
 
     my $is_valid = is_valid_line();
     if ($is_valid == 1) {
       my $start = 0;
       my $index = 0;
-      foreach my $size (@column_new_size) {
+
+      my $max_sizes = @column_max_size[$current_result - 1];
+      my $new_sizes = @column_new_size[$current_result - 1];
+      foreach my $size (@$new_sizes) {
         my $current_column = substr($line, $start, $size);
 
         if ($index != 0) {
@@ -173,7 +212,7 @@ while(<INPUT>) {
         }
         print " $current_column ";
 
-        $start += $column_max_size[$index] + 1;
+        $start += @$max_sizes[$index] + 1;
         $index++;
       }
 
